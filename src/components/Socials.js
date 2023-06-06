6b0a3007348927661ee747cb9d3f245db8ab91d2
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-07 01:47:51 +0300

    Implemented menu navigation, icons and effects

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,27 @@
 import React from 'react';
+// import icons
+import {
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube,
+} from 'react-icons/im'
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+  <ul className='flex gap-x-4'>
+    <li><a href="https://www.facebook.com/isportphoto" target="_blank">
+      <ImFacebook />
+    </a>
+    </li>
+    <li><a href="https://www.instagram.com/isport.photo" target="_blank">
+      <ImInstagram />
+    </a>
+    </li>
+
+  </ul>
+  </div>
 };
 
 export default Socials;
