4e4c2eaae503099f3cf143d9c17c81d8bb02ea97
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-07 22:18:05 +0300

    Prepare About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,22 @@ import WomanImg from '../img/about/woman.png';
 import { Link } from 'react-router-dom';
 
 const About = () => {
-  return <section className='section bg-yellow-200'>About</section>;
+  return <section className='section '>
+    <div className='container mx-auto h-full relative'>
+      {/** text & image wrapper */}
+      <div className='bg-pink-200 flex flex-col lg:flex-row
+      h-full items-center justify-center gap-x-24 text-center lg:text-left '>
+        {/** image */}
+        <div>image</div>
+        {/** text */}
+        <div>
+          text
+        </div>
+
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default About;
