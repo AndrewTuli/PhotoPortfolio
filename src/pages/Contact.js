da2ecb818320f32150d1942b94d958aedd2f9548
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-08 00:17:32 +0300

    Started implementing Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react';
+// import images
+import WomanImg from '../img/contact/woman.png';
+//import AboutMeImg from '../img/about/aboutme.png';
 
 const Contact = () => {
-  return <section className='section bg-amber-600'>Contact</section>;
+  return <section className='section bg-white'>
+    
+    Contact
+  
+  
+  </section>;
 };
 
 export default Contact;
+ 
