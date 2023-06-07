f3e1f545f57f649860fc6ca6b5d5854e3870a4aa
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-07 21:57:04 +0300

    Started implementing About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,9 @@
 import React from 'react';
+// import images
+import WomanImg from '../img/about/woman.png';
+//import IamPhotographer from '../img/about/iamphotographer2.png';
+// import link
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return <section className='section bg-yellow-200'>About</section>;
