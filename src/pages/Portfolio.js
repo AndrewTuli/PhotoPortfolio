b028e6b1b99ee4358f79a14a40744dd5d62df5dd
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-07 23:57:25 +0300

    Created image grid for Portfollio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,9 +6,101 @@ import Image3 from '../img/portfolio/3.png'
 import Image4 from '../img/portfolio/4.png'
 import Image5 from '../img/portfolio/5.png'
 import Image6 from '../img/portfolio/6.png'
+// import link
+import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
-  return <section className='section bg-green-200'>Portfolio</section>;
+  return <section className='section'>
+    <div className='container mx-auto h-full relative'>
+      <div className='flex flex-col lg:flex-row h-full 
+      items-center justify-start gap-x-24 text-center
+      lg:text-left pt-24 lg:pt-36 pb-8'>
+        {/* text */}
+        <div className='flex flex-col lg:item-start'>
+          <h1 className='h1'>Portfolio</h1>
+          <p className='mb-12 max-w-sm'>
+            Welcome to my world of capturing moments and preserving memories!
+            <br />
+            <br />
+            I am a sports photographer with a passion for immortalizing
+            the essence of sports events, extreme sports, and the vibrancy of active life.
+            From speed and agility to strength and victory, I strive to freeze those
+            fleeting seconds in time. I also love skiing, mountain biking, wildlife,
+            and travel.
+            <br />
+            <br />
+            Let me find interesting places for you to capture your moments of joy and happiness. Join me on this visual voyage, one click at a time.
+          </p>
+          <Link to={'/contact'} className='btn mb-[30px]
+          mx-auto lg:mx-0'>
+            Hire me
+          </Link>
+        </div>
+        {/* image grid */}
+        <div className='grid grid-cols-2 lg:gap-2'>
+          {/* image */}         
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image1} 
+            alt=''
+            />
+          </div>
+
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image2} 
+            alt=''
+            />
+          </div>
+
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image3} 
+            alt=''
+            />
+          </div>
+
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image4} 
+            alt=''
+            />
+          </div>
+
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image5} 
+            alt=''
+            />
+          </div>
+
+          <div className='max-w-[250px] lg:max-w-[320px] 
+          h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+            <img className='object-cover h-full lg:h-[220px] 
+            hover:scale-110 transition-all duration-500'  
+            src={Image6} 
+            alt=''
+            />
+          </div>
+
+
+
+
+        </div>
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default Portfolio;
