967df309d5d3fe41b246d37428a15ec56b12a0e5
Andrew Tulchinski <131363819+AndrewTuli@users.noreply.github.com>
2023-06-08 01:48:15 +0300

    Fix Navigation menu in the Header. Fix AnimRoutes function

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -7,15 +7,21 @@ import Contact from '../pages/Contact';
 
 // import routes route & useLocation hook
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const AnimRoutes = () => {
-  return <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/about' element={<About/>} />
-    <Route path='/portfolio' element={<Portfolio/>} />
-    <Route path='/contact' element={<Contact/>} />
+  const location = useLocation();  
+  return (
+    <AnimatePresence initial={true} mode='wait'>
+      <Routes key={location.pathname} location={location}>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
 
-  </Routes>
+      </Routes>
+    </AnimatePresence>
+  )
 };
 
 export default AnimRoutes;
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,41 @@ import MobileNav from './MobileNav';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <header className='fixed w-full px-[30px] 
-  lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
-    <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
-      {/* logo [width = 200px] */}
-      <Link to={'/'} className='max-w-[200px]'>
-        <img src={Logo} alt='' /> 
-      </Link> 
-      
-      {/* nav initially hidden - show on desktop site (route path described at AnimRoutes.js) */}
-      <nav className='hidden xl:flex gap-x-12 font-semibold'>
-        <Link to={'/'} 
-        className='text-[#696c6d] hover:text-primary transition'>
-          Home</Link> 
-        <Link to={'/about'} 
-        className='text-[#696c6d] hover:text-primary transition'>
-          About</Link> 
-        <Link to={'/portfolio'} 
-        className='text-[#696c6d] hover:text-primary transition'>
-          Portfolio</Link> 
-        <Link to={'/contact'} 
-        className='text-[#696c6d] hover:text-primary transition'>
-          Contact</Link> 
-      </nav>
+  return (
+    <header className='fixed w-full px-[30px] 
+    lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+      <div className='flex flex-col lg:flex-row 
+      lg:items-center w-full justify-between'>
+        {/* logo [width = 200px] */}
+        <Link to={'/'} className='max-w-[200px]'>
+          <img src={Logo} alt='' />
+        </Link>
+
+        {/* nav initially hidden - show on desktop site (route path described at AnimRoutes.js) */}
+        <nav className='hidden xl:flex gap-x-12 
+        font-semibold'>
+          <Link to={'/'}
+            className='text-[#696c6d] hover:text-primary transition'>
+            Home</Link>
+          <Link to={'/about'}
+            className='text-[#696c6d] hover:text-primary transition'>
+            About</Link>
+          <Link to={'/portfolio'}
+            className='text-[#696c6d] hover:text-primary transition'>
+            Portfolio</Link>
+          <Link to={'/contact'}
+            className='text-[#696c6d] hover:text-primary transition'>
+            Contact</Link>
+        </nav>
+      </div>
       
       {/* socials */}
-      <Socials/>
+      <Socials />
 
       {/* mobile nav */}
-      <MobileNav/>
-
-
-    </div>
-  </header>;
+      <MobileNav />
+    </header>
+  );
 };
 
 export default Header;
